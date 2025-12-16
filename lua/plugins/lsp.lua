079a6a2dac50fcf6cb7df57b152363300b048701
ethanshamilton <ethan.hamilton798@gmail.com>
2025-12-15 23:04:16 -0500

    switch to ty for type checking

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,31 +6,33 @@ return {
         enabled = false,
       },
       servers = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              -- Set to "basic" for less strict checking
-              typeCheckingMode = "basic", -- or "standard" or "off"
-
-              -- Disable specific annoying diagnostics
-              diagnosticMode = "openFilesOnly", -- don't check whole workspace
-
-              -- Turn off specific rules
-              disableOrganizeImports = false,
-              analysis = {
-                -- Ignore these specific diagnostics
-                diagnosticSeverityOverrides = {
-                  reportMissingTypeStubs = "none",
-                  reportUnknownMemberType = "none",
-                  reportUnknownArgumentType = "none",
-                  reportUnknownVariableType = "none",
-                  reportUnknownParameterType = "none",
-                  reportMissingParameterType = "none",
-                },
-              },
-            },
-          },
-        },
+        ty = {},
+        basedpyright = false,
+        -- basedpyright = {
+        --   settings = {
+        --     basedpyright = {
+        --       -- Set to "basic" for less strict checking
+        --       typeCheckingMode = "basic", -- or "standard" or "off"
+        --
+        --       -- Disable specific annoying diagnostics
+        --       diagnosticMode = "openFilesOnly", -- don't check whole workspace
+        --
+        --       -- Turn off specific rules
+        --       disableOrganizeImports = false,
+        --       analysis = {
+        --         -- Ignore these specific diagnostics
+        --         diagnosticSeverityOverrides = {
+        --           reportMissingTypeStubs = "none",
+        --           reportUnknownMemberType = "none",
+        --           reportUnknownArgumentType = "none",
+        --           reportUnknownVariableType = "none",
+        --           reportUnknownParameterType = "none",
+        --           reportMissingParameterType = "none",
+        --         },
+        --       },
+        --     },
+        --   },
+        -- },
       },
     },
   },
